413ae95721f916073c6440541eb029b96cca3529
Pedro Eguez <pejosec@hotmail.com>
2023-01-20 09:45:49 -0500

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,35 @@ export class InfoPaginaService {
   info: InfoPagina  = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-  //  console.log('Servicio de infoPagina cargado');
-    
-    //Leer el archivo JSON
-    this.http.get('/assets/data/data-pagina.json')
+    this.cargarInfo();   
+    this.cargarEquipo(); 
+  
+  }
+
+  private cargarInfo() {
+      //Leer el archivo JSON
+      this.http.get('/assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+        // console.log(resp);
 
       });
+  }
+
+  private cargarEquipo() {
+    //Llamar al servicio web y sacar su informacion
+    this.http.get('https://angular-html-b6bea-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe ( (respEquipo : any) => {
+        this.equipo = respEquipo;
+        // console.log(this.equipo);
+
+    });
+
   }
 }
